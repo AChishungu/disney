31720a7f03a50bf30481153dfd7ab0b493b9815f
AChishungu <72464199+AChishungu@users.noreply.github.com>
2021-03-07 23:09:45 -0800

    Update disney-year.js

--- data/disney-year.js
+++ data/disney-year.js
@@ -93,18 +93,18 @@
 	"Image":"pocahontas.jpg"
 	},
 	{
-			"Film":8,
-			"Title":"Mulan",
-			"Year":1998,
-			"Director":"Barry Cook",
-			"Producers":"Pam Coats",
-			"Writers":"Robert D. San Souci",
-			"Composer":"John Barry",
-			"Princess":"Mulan",
-			"Budget":"$90 million",
-			"BoxOffice":"$304.3 million",
-			"Image":"mulan.jpg"
-	},
+	"Film":8,
+	"Title":"Mulan",
+	"Year":1998,
+	"Director":"Barry Cook",
+	"Producers":"Pam Coats",
+	"Writers":"Robert D. San Souci",
+	"Composer":"John Barry",
+	"Princess":"Mulan",
+	"Budget":"$90 million",
+	"BoxOffice":"$304.3 million",
+	"Image":"Mulan.jpg"
+    },
 	{
 	"Film":9,
 	"Title":"The Princess and the Frog",
